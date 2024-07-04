84db12edfda9cf021b066d90967d8032c568c81f
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-07-04 17:18:44 +0200

    Move sorted elements into sorted_arr and return it

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,15 @@
 def bubble_sort(unsorted_arr)
   sorted_arr = []
   while unsorted_arr.size > 0
+    sorted_counter = 0
     unsorted_arr.each_with_index do |number, index|
+      sorted_counter += 1
       if number > unsorted_arr[index+1]
         unsorted_arr[index], unsorted_arr[index+1] = unsorted_arr[index+1], unsorted_arr[index]
+        sorted_counter = 0
       end
     end
+    sorted_counter.times {sorted_arr.unshift(unsorted_arr.pop())}
   end
+  sorted_arr
 end
