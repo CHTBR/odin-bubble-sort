b51198eb686e7273f817ae8d60ef6182666ffcb8
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-07-04 17:10:27 +0200

    Swap neighbor values to push the bigger further

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,7 +2,9 @@ def bubble_sort(unsorted_arr)
   sorted_arr = []
   while unsorted_arr.size > 0
     unsorted_arr.each_with_index do |number, index|
-
+      if number > unsorted_arr[index+1]
+        unsorted_arr[index], unsorted_arr[index+1] = unsorted_arr[index+1], unsorted_arr[index]
+      end
     end
   end
 end
